98c9bd70a3e9f0f7eb131b0a749d7dd0e7f5a323
MP Web Developer <mprwdev@gmail.com>
2025-09-29 21:59:36 +0300

    use module

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("test check");
+import { apiKey, apiKey2 as apiKeyNew2 } from "./utils.js";
+import * as util from "./utils.js";
+
+import newApiKey from "./utils.js";
+
+console.log(apiKey);
+console.log(apiKeyNew2);
+console.log(newApiKey);
+//
+console.log("--------");
+//
+console.log(util.apiKey);
+console.log(util.apiKey2);
+console.log(util.default);
---
+++ js/utils.js
@@ -0,0 +1,3 @@
+export let apiKey = "dfasdfadsgf234afdg";
+export let apiKey2 = "2zzzdfasdfadsgf234afdg";
+export default "barabaasdfadsf";
