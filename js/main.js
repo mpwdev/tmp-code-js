8aa7c72a505418468a2c0ba4977fb8b5d0ba9032
MP Web Developer <mprwdev@gmail.com>
2025-10-01 21:09:13 +0300

    tmp dom query methods

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const listItemFirst = document.querySelector(".list-item");
+console.log(listItemFirst);
+
+const listItems = document.querySelectorAll(".list-item");
+console.log(listItems);
+
+const liFirstOfType = document.querySelector("ul li:first-of-type");
+console.log(liFirstOfType);
+
+const ul = document.querySelector("ul");
+const firstLi = ul.querySelector("li");
+console.log(firstLi);
